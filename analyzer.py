f79bdab6659fde3fc35d2df5a3e86d5116b10d65
Azaucifer <47237500+Azaucifer@users.noreply.github.com>
2026-09-02 13:00:38 +0530

    Improve TODO and FIXME detection

--- analyzer.py
+++ analyzer.py
@@ -2,6 +2,8 @@ from pathlib import Path
 import ast
 import json
 import argparse
+import tokenize
+from io import StringIO
 
 
 def generate_json_report(results, output_file="codebase_report.json"):
@@ -176,11 +178,29 @@ def parse_python_file(source, file):
         return None
 
 
+def count_todos_and_fixmes(lines):
+    """Count TODO and FIXME markers in Python comments."""
+    source = "\n".join(lines)
+
+    todos = 0
+    fixmes = 0
+
+    tokens = tokenize.generate_tokens(StringIO(source).readline)
+
+    for token in tokens:
+        if token.type == tokenize.COMMENT:
+            if "TODO" in token.string:
+                todos += 1
+
+            if "FIXME" in token.string:
+                fixmes += 1
+
+    return todos, fixmes
+
+
 def analyze_lines(lines):
     blank_lines = 0
     comment_lines = 0
-    todos = 0
-    fixmes = 0
 
     for line in lines:
         checked_line = line.strip()
@@ -188,10 +208,8 @@ def analyze_lines(lines):
             blank_lines += 1
         if checked_line.startswith("#"):
             comment_lines += 1
-        if "TODO" in checked_line:
-            todos += 1
-        if "FIXME" in checked_line:
-            fixmes += 1
+
+    todos, fixmes = count_todos_and_fixmes(lines)
 
     code_lines = len(lines) - blank_lines - comment_lines
 
